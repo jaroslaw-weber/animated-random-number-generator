3de1b73768d9ab969530e7462554f8d431e1a2e6
Jaroslaw Weber <jaroslaw.weber@gmail.com>
2025-09-14 08:50:40 +0900

    add electro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  base: '/animated-random-number-generator', // Add this line
+  integrations: [preact()],
+  base: "",
 });
